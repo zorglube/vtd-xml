9b5749dcf8db2852b6343b6d94c70593ba9609f8
Zorglube <630192+zorglube@users.noreply.github.com>
2026-03-10 17:35:49 +0100

    First try of migration to Git and Maven
    Signed-off-by: Zorglube <630192+zorglube@users.noreply.github.com>

---
+++ ximple-dev/src/test/java/DOMTest/test7.java
@@ -0,0 +1,54 @@
+package DOMTest;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
+public class test7 {
+    static Runtime rt;
+
+    public static void main(String[] args) {
+
+        // File f = new File(args[0]);
+        File f = new File("c:/xml/test11.xml");
+
+        try {
+
+            FileInputStream fis = new FileInputStream(f);
+            rt = Runtime.getRuntime();
+            byte[] ba = new byte[(int) f.length()];
+
+            fis.read(ba);
+            // vg.setDoc(ba);
+            long startMem = rt.totalMemory() - rt.freeMemory();
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setFeature("http://apache.org/xml/features/dom/defer-node-expansion", false);
+            factory.setNamespaceAware(true);
+            factory.setExpandEntityReferences(false);
+
+            DocumentBuilder parser = factory.newDocumentBuilder();
+            ByteArrayInputStream bais = new ByteArrayInputStream(ba);
+            Document d = parser.parse(bais);
+            Node n = d.getElementsByTagNameNS(null, "abc").item(0);
+            System.out.println(n);
+            NamedNodeMap n1 = n.getAttributes();
+            Node n2 = n1.getNamedItemNS(null, "abc");
+            // Node n1 = n.
+            System.out.println(n2);
+            // Node n1 = n.
+
+            long endMem = rt.totalMemory() - rt.freeMemory();
+            System.out.println("Memory Use: " + ((float) endMem - startMem) / (1 << 20) + " MB.");
+            System.out.println("Multiplying factor: " + ((float) endMem - startMem) / f.length());
+        } catch (Exception e) {
+            System.out.println("exception ==> " + e);
+        }
+    }
+}
